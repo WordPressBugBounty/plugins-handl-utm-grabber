87f6a4b572dc663ecbf8a1602aab666a05d11e0c
Sean Murphy <sean@iamseanmurphy.com>
2026-06-22 21:52:46 +0000

    2.9.4

--- handl-utm-grabber/external/ninja.php
+++ handl-utm-grabber/external/ninja.php
@@ -11,37 +11,37 @@ class HandLUTM_MergeTags extends NF_Abstracts_MergeTags
   public function __construct()
   {
     parent::__construct();
-    
-    /* Translatable display name for the group. */
-    $this->title = __( 'HandL UTM Grabber', 'ninja-forms' );
-    
-    /* Individual tag registration. */
-    
+
+    /* Plain strings until init; translations must not run before init (WP 6.7+). */
+    $this->title = 'HandL UTM Grabber';
+
     $my_merge_tags = array();
     $fields = array('utm_source','utm_medium','utm_term', 'utm_content', 'utm_campaign', 'gclid', 'handl_original_ref', 'handl_landing_page', 'handl_ip', 'handl_ref', 'handl_url');
     foreach ($fields as $field){
     	$my_merge_tags[$field] = array(
           'id' => $field,
-          'tag' => '{handl:'.$field.'}', // The tag to be  used.
-          'label' => __( $field, 'handl_utm_grabber' ), // Translatable label for tag selection.
-          // Read at tag use.
+          'tag' => '{handl:'.$field.'}',
+          'label' => $field,
           'callback' => function() use ($field) {
               return isset($_COOKIE[$field]) ? esc_html($_COOKIE[$field]) : '';
           }
         );
     }
-    
+
     $this->merge_tags = $my_merge_tags;
-    
-    /*
-     * Use the `init` and `admin_init` hooks for any necessary data setup that relies on WordPress.
-     * See: https://codex.wordpress.org/Plugin_API/Action_Reference
-     */
+
     add_action( 'init', array( $this, 'init' ) );
     add_action( 'admin_init', array( $this, 'admin_init' ) );
   }
-  
-  public function init(){ /* This section intentionally left blank. */ }
+
+  public function init() {
+    $this->title = __( 'HandL UTM Grabber', 'ninja-forms' );
+
+    foreach ( array_keys( $this->merge_tags ) as $field ) {
+      $this->merge_tags[ $field ]['label'] = __( $field, 'handl_utm_grabber' );
+    }
+  }
+
   public function admin_init(){ /* This section intentionally left blank. */ }
   
 }
--- handl-utm-grabber/handl-utm-grabber.php
+++ handl-utm-grabber/handl-utm-grabber.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Plugin Name: HandL UTM Grabber
  * Description: The easiest way to capture UTMs on your (optin) forms.
  * Author: Haktan Suren
- * Version: 2.9.3
+ * Version: 2.9.4
  * Author URI: https://www.utmgrabber.com/
 */
 
